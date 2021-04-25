035b28e887c3796e6d7ccb3c442829c6f6a00ae4
Jin Zhang <janetzha@gmail.com>
2021-04-25 09:56:49 -0400

    robo react app

---
+++ src/components/SearchBox.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react';
+
+const SearchBox = ({searchChange}) => {
+    return (
+        <>
+        <input
+            className='pa3 ba b--green bg-lightest-blue'
+            type='search' 
+            placeholder='search robots'
+            onChange={ searchChange }
+        />
+        </>
+    );
+}
+
+export default SearchBox;
